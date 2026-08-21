c13656b089d7c8dee9c6b00fcab4161e0eaffea0
Earvin Lin(ASUSWinPC) <lin.earvin@gmail.com(ASUSWinPC)>
2026-08-21 21:03:31 +0800

    20260821-2103 add files.

---
+++ EarvinStocksPGM_CS/EarvinStocksPGM_CS/Form1.cs
@@ -0,0 +1,15 @@
+namespace EarvinStocksPGM_CS
+{
+    public partial class frmEarvinStocks : Form
+    {
+        public frmEarvinStocks()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            btnTest.Left += 100;
+        }
+    }
+}
