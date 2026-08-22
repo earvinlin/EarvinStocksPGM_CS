858f3a0651776ba641fb599d5360c2a89fe3b077
Earvin Lin(ASUSWinPC) <lin.earvin@gmail.com(ASUSWinPC)>
2026-08-22 17:33:51 +0800

    20260822-1733 add files.

---
+++ StocksTestPGM/StocksTestPGM/StocksTestPGM/frmStocksTest.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace StocksTestPGM
+{
+    partial class frmStocksTest
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTest = new Button();
+            SuspendLayout();
+            // 
+            // btnTest
+            // 
+            btnTest.Location = new Point(250, 155);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(91, 36);
+            btnTest.TabIndex = 0;
+            btnTest.Text = "左移我";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click;
+            // 
+            // frmStocksTest
+            // 
+            AutoScaleDimensions = new SizeF(10F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 600);
+            Controls.Add(btnTest);
+            Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "frmStocksTest";
+            Text = "Stocks Test Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnTest;
+    }
+}
---
+++ StocksTestPGM/StocksTestPGM/StocksTestPGM/frmStocksTest.cs
@@ -0,0 +1,15 @@
+namespace StocksTestPGM
+{
+    public partial class frmStocksTest : Form
+    {
+        public frmStocksTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            btnTest.Left += 100;
+        }
+    }
+}
