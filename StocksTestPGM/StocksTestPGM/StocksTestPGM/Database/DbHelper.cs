1f3e3c8686b3f0a098e515ce92402c6a364ff5c2
Earvin Lin(ASUSWinPC) <lin.earvin@gmail.com(ASUSWinPC)>
2026-08-25 21:56:15 +0800

    20260825-2156 add files.

---
+++ StocksTestPGM/StocksTestPGM/StocksTestPGM/Database/DbHelper.cs
@@ -0,0 +1,13 @@
+﻿using MySqlConnector;
+using System;
+
+public static class DbHelper
+{
+    private static string ConnStr = "Server=localhost;Database=stocksdb;User ID=root;Password=lin32ledi;";
+           
+public static MySqlConnection GetConnection()
+    {
+        using var conn = new MySqlConnection(ConnStr);
+        return new MySqlConnection(ConnStr);
+    }
+}
