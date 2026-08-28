f329c8ff7bfbe4d72408f00eccd4462153dbcf6b
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2026-08-28 17:19:58 +0800

    20260828-1719 add files.

--- EarvinStocksPGM/EarvinStocksPGM/frmStocksPGM.cs
+++ EarvinStocksPGM/EarvinStocksPGM/frmStocksPGM.cs
@@ -47,33 +47,6 @@ namespace EarvinStocksPGM
 
         private void btnFocus_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show($"寬度: {this.Width}, 高度: {this.Height}");
-            //MessageBox.Show($"寬度: {this.ClientSize.Width}, 高度: {this.ClientSize.Height}");
-
-            //string connStr =
-            //    "Server=localhost;Database=stocksdb;User ID=root;Password=lin32ledi;";
-
-            //using var conn = new MySqlConnection(connStr);
-
-            //try
-            //{
-            //    conn.Open();
-            //    Console.WriteLine("連線成功");
-            //    MessageBox.Show("連線成功");
-            //    string sql = "SELECT DATE, START_PRICE, HIGH_PRICE, LOW_PRICE, END_PRICE, VOLUME FROM TAIWAN_DATA_POLARIS WHERE STOCK_NO = @stock_no ORDER BY DATE ";
-            //    MySqlCommand cmd = new MySqlCommand(sql, conn);
-            //    cmd.Parameters.AddWithValue("@stock_no", "1101");
-            //    MySqlDataReader reader = cmd.ExecuteReader();
-            //    while (reader.Read())
-            //    {
-            //        System.Diagnostics.Debug.WriteLine($"{reader["DATE"]}, " + $"{reader["START_PRICE"]}, " + $"{reader["END_PRICE"]}");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"連線失敗: {ex.Message}");
-            //    MessageBox.Show("連線失敗");
-            //}
             DbHelper.TestConnectDB();
         }
 
